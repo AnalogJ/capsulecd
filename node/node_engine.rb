b61e14b1a2b1c05e444a6c56c1c4e1b065e87a7d
Jason Kulatunga <jk17@ualberta.ca>
2016-02-16 21:25:50 -0800

    rescue when we cant commit local changes, will usually fail if nothing change.d

--- node/node_engine.rb
+++ node/node_engine.rb
@@ -80,7 +80,7 @@ class NodeEngine < Engine
 
     #commit changes to the cookbook. (test run occurs before this, and it should clean up any instrumentation files, created,
     # as they will be included in the commmit and any release artifacts)
-    GitUtils.commit(@source_git_local_path, 'Committing automated changes before packaging.')
+    GitUtils.commit(@source_git_local_path, 'Committing automated changes before packaging.') rescue puts 'Could not commit changes locally..'
 
     # run npm publish
     Open3.popen3('npm version patch -m "(v%s) Automated packaging of release by CapsuleCD"', :chdir => @source_git_local_path) do |stdin, stdout, stderr, external|
