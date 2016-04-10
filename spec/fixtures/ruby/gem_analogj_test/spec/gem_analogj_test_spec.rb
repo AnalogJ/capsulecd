326c87f9b21a73658a061f73f23f9842f32b7ac6
Jason Kulatunga <jk17@ualberta.ca>
2016-04-10 11:52:56 -0700

    simplify test.

--- spec/fixtures/ruby/gem_analogj_test/spec/gem_analogj_test_spec.rb
+++ spec/fixtures/ruby/gem_analogj_test/spec/gem_analogj_test_spec.rb
@@ -1,10 +1,6 @@
 require 'spec_helper'
 
 describe 'GemTest' do
-  it 'has a version number' do
-    expect(GemTest::VERSION).not_to be nil
-  end
-
   it 'does something useful' do
     expect(true).to eq(true)
   end
