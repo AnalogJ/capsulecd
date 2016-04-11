127691d95e6d841b61e0f9ce4ef75289da0e6880
Jason Kulatunga <jk17@ualberta.ca>
2016-04-11 16:46:09 -0700

    added tests to validation utils and default runner.

--- lib/capsulecd/base/common/validation_utils.rb
+++ lib/capsulecd/base/common/validation_utils.rb
@@ -1,9 +1,11 @@
 require 'capsulecd'
 module CapsuleCD
   class ValidationUtils
+    # TODO: validation almost needs to be source specific (or inherit from this base function), because source methods
+    # may require additional attributes, while these base payload keys are required for general step functions.
     def self.validate_repo_payload(repo_payload)
       unless repo_payload['sha']
-        fail CapsuleCD::Error::SourcePayloadFormatError, 'Incorrectly formatted payload, missing "sha1" key'
+        fail CapsuleCD::Error::SourcePayloadFormatError, 'Incorrectly formatted payload, missing "sha" key'
       end
       unless repo_payload['ref']
         fail CapsuleCD::Error::SourcePayloadFormatError, 'Incorrectly formatted payload, missing "ref" key'
