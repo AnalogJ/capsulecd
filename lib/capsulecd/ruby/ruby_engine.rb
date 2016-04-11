10f4b7d0a21f685052e18b10e6ba1a535577cef0
Jason Kulatunga <jk17@ualberta.ca>
2016-04-11 12:28:11 -0700

    added helper spec file.

--- lib/capsulecd/ruby/ruby_engine.rb
+++ lib/capsulecd/ruby/ruby_engine.rb
@@ -11,10 +11,10 @@ module CapsuleCD
         super
         gemspec_path = CapsuleCD::Ruby::RubyHelper.get_gemspec_path(@source_git_local_path)
 
-        # check for/create required VERSION file
-        gemspec_data = CapsuleCD::Ruby::RubyHelper.load_gemspec_data(gemspec_path)
+        # check for required VERSION file
+        gemspec_data = CapsuleCD::Ruby::RubyHelper.get_gemspec_data(@source_git_local_path)
 
-        if !gemspec_data || !File.exist?(CapsuleCD::Ruby::RubyHelper.version_filepath(@source_git_local_path, gemspec_data.name))
+        if !File.exist?(CapsuleCD::Ruby::RubyHelper.version_filepath(@source_git_local_path, gemspec_data.name))
           fail CapsuleCD::Error::BuildPackageInvalid, 'version.rb file is required to process Ruby gem'
         end
 
