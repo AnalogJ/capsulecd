a3f36281bddff340c6e26c890cc865a3916c85e6
Jason Kulatunga <jk17@ualberta.ca>
2017-07-22 19:56:21 -0700

    use gocovmerge to upload to codecov.

--- ci/coverage.sh
+++ ci/coverage.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 
 set -e
-echo "" > coverage.txt
+echo "" > /coverage/coverage-$1.txt
 
 for d in $(go list ./... | grep -v vendor); do
-    go test -race -coverprofile=profile.out -covermode=atomic "$@" $d
+    go test -race -coverprofile=profile.out -covermode=atomic -tags="static $1" $d
     if [ -f profile.out ]; then
-        cat profile.out >> coverage.txt
+        cat profile.out >> /coverage/coverage-$1.txt
         rm profile.out
     fi
 done
-
-# upload to codecov.
-bash <(curl -s https://codecov.io/bash)
--- pkg/engine/engine_base_impl_test.go
+++ pkg/engine/engine_base_impl_test.go
@@ -21,3 +21,35 @@ func TestEngineBase_BumpVersion(t *testing.T) {
 	//assert
 	require.Equal(t, ver, "1.2.3", "should correctly do a patch bump")
 }
+
+func TestEngineBase_BumpVersion_InvalidCurrentVersion(t *testing.T) {
+
+	//setup
+	testConfig, _ := config.Create()
+	eng := engineBase{
+		Config: testConfig,
+	}
+
+	//test
+	nextV, err := eng.BumpVersion("abcde")
+
+	//assert
+	require.Error(t, err, "should return an error if unparsable version")
+	require.Nil(t, nextV, "should be nil next version")
+}
+
+func TestEngineBase_BumpVersion_WithVPrefix(t *testing.T) {
+
+	//setup
+	testConfig, _ := config.Create()
+	eng := engineBase{
+		Config: testConfig,
+	}
+
+	//test
+	nextV, err := eng.BumpVersion("v1.2.3")
+	require.Nil(t, err)
+
+	//assert
+	require.Equal(t, nextV, "1.2.4", "should correctly do a patch bump")
+}
