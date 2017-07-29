f258c680346214f6f125bd514c7f963423dfa29c
Jason Kulatunga <jk17@ualberta.ca>
2017-07-29 10:20:40 -0700

    updating base image for Cross Compliation to windows and Mac.

---
+++ ci/capsulecd.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# retrieve the latest capsulecd release info
+asset_url=$(curl -s https://api.github.com/repos/AnalogJ/capsulecd/releases/latest \
+	| grep browser_download_url | grep 'capsulecd' | cut -d '"' -f 4)
+
+# download the capsulecd asset here.
+curl -L -o capsulecd $asset_url
+
+# make capsulecd executable
+chmod +x capsulecd
