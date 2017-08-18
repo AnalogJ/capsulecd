f01562d32d2517a160e6a3bf2e845c5dcd6f5e8e
CapsuleCD <CapsuleCD@users.noreply.github.com>
2017-08-18 22:56:56 +0000

    (v2.0.14) Automated packaging of release by CapsuleCD

--- pkg/scm/scm_github.go
+++ pkg/scm/scm_github.go
@@ -271,8 +271,7 @@ func (g *scmGithub) Publish() error {
 	parts := strings.Split(g.Config.GetString("scm_repo_full_name"), "/")
 	version := fmt.Sprintf("v%s", g.PipelineData.ReleaseVersion)
 
-
-	log.Printf("Creating new release for `%s/%s` with version: `%s` on commit: `%s`. Commit message: `%s`", parts[0],parts[1], version, releaseSha, releaseBody )
+	log.Printf("Creating new release for `%s/%s` with version: `%s` on commit: `%s`. Commit message: `%s`", parts[0], parts[1], version, releaseSha, releaseBody)
 
 	releaseData, _, rerr := g.Client.Repositories.CreateRelease(
 		ctx,
