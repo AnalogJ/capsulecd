76897712260fcf1d1a6cf5093fce170b9f37bf7f
Jason Kulatunga <jason@thesparktree.com>
2019-11-17 08:57:39 -0800

    bump go build environment to 1.13.4

--- ci/development.sh
+++ ci/development.sh
@@ -3,7 +3,7 @@
 # instructions taken from capsule.yml (we only care about a linux development environment right now)
 cd /go/src/github.com/analogj/capsulecd
 rm -rf vendor
-dep ensure
+go mod vendor
 
 mkdir -p vendor/gopkg.in/libgit2/git2go.v25/vendor/libgit2/build/
 cp -r /usr/local/linux/lib/pkgconfig/. /go/src/github.com/analogj/capsulecd/vendor/gopkg.in/libgit2/git2go.v25/vendor/libgit2/build/
