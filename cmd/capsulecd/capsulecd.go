292dd038b14d254ed88967846d83410a6028b69c
Jason Kulatunga <jason@thesparktree.com>
2019-11-17 11:44:36 -0800

    removing dep, migrating officially to go mod.

--- cmd/capsulecd/capsulecd.go
+++ cmd/capsulecd/capsulecd.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"time"
 
@@ -125,5 +126,8 @@ func main() {
 		},
 	}
 
-	app.Run(os.Args)
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatalf("ERROR: %v", err)
+	}
 }
